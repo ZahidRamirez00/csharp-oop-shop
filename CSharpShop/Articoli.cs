efb4615cdcd7c3ddb1b1c45994f7a53569d831c4
ZahidRamirez00 <zahidramirez.00@gmail.com>
2022-11-29 17:21:09 +0100

    Prova se la variabile codice nella classe Articoli è solo leggibile e non modificabile

--- CSharpShop/Articoli.cs
+++ CSharpShop/Articoli.cs
@@ -8,5 +8,71 @@ namespace CSharpShop
 {
     internal class Articoli
     {
+        private int codice;
+        private string nome;
+        private string descrizione;
+        private int prezzo;
+        private int iva;
+
+        public int GetCodice()
+        {
+            return this.codice;
+        }
+
+        /*public void SetCodice(int Codice)
+        {
+            this.codice = Codice;
+        }*/
+
+        public string GetNome()
+        {
+            return this.nome;
+        }
+        
+        public void SetNome(string Nome)
+        {
+            this.nome = Nome;
+        }
+
+        public string GetDescrizione()
+        {
+            return this.descrizione;
+        }
+
+        public void SetDescrizione(string Descrizione)
+        {
+            this.descrizione = Descrizione;
+        }
+
+        public int GetPrezzo()
+        {
+            return this.prezzo;
+        }
+
+        public void SetPrezzo(int Prezzo)
+        {
+            this.prezzo = Prezzo;
+        }
+
+        public int GetIva()
+        {
+            return this.iva;
+        }
+
+        public void SetIva(int Iva)
+        {
+            this.iva = Iva;
+        }
+
+        // Costuttore che definisco IO e che usa il concetto di OVERLOAD
+        public Articoli(int Codice, string Nome, string Descrizione, int Prezzo, int Iva)
+        {
+            this.codice = Codice;
+            this.nome = Nome;
+            this.descrizione = Descrizione;
+            this.prezzo = Prezzo;
+            this.iva = Iva;
+        }
+
     }
 }
--- CSharpShop/Program.cs
+++ CSharpShop/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using CSharpShop;
+
+Articoli prodotto;
+string myString = "";
+int codice;
+string nome;
+string descrizione = "";
+int prezzo;
+int iva;
+bool controllo;
+
+Random codiceRandom = new Random();
+codice = codiceRandom.Next(1, 99999999);
+
+Console.Write("Inserisci il nome del prodotto: ");
+nome= Console.ReadLine();
+
+Console.Write("Inserisci una descrizione del prodotto: ");
+descrizione= Console.ReadLine();
+
+Console.Write("Inserisci il prezzo del prodotto: ");
+do
+{
+    myString = Console.ReadLine();
+    if (int.TryParse(myString, out prezzo) )
+    {
+        controllo = true;
+    }
+    else
+    {
+        Console.WriteLine("\nHai inserito un valore errato!!!");
+        controllo = false;
+        Console.Write("\nInserisci UN NUMERO per define il prezzo del prodotto: ");
+    }
+} while (!controllo);
+
+Console.Write("Inserisci la percentuale dell'IVA: ");
+do
+{
+    myString = Console.ReadLine();
+    if (int.TryParse(myString, out iva))
+        if (iva > 0 && iva <= 100)
+        {
+        controllo = true;
+        }
+        else 
+        {
+            Console.WriteLine("\nHai inserito un valore errato!!!");
+            controllo = false;
+            Console.Write("\nInserisci UN NUMERO 0 a 100 per definire la percentuale dell'IVA: ");
+        }
+} while (!controllo);
+
+prodotto = new Articoli(codice, nome, descrizione, prezzo, iva);
+
+Console.WriteLine(prodotto);
